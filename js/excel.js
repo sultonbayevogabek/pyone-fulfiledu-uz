1c233488aace0f5d7ee9fb81802ca18ebcd0644e
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-03-16 16:49:04 +0500

    commit

--- js/excel.js
+++ js/excel.js
@@ -1,20 +1,34 @@
 'use strict';
 
-let user = JSON.parse(localStorage.getItem('user'));
+(async _ => {
+  const statistics = new Statistics();
+  let user = JSON.parse(localStorage.getItem('user'));
 
-if (user && user?.name && user?.phone && user?.time) {
+  document.querySelector('#joinToChannel')?.addEventListener('click', async (e) => {
+    e.target.setAttribute('disabled', 'true');
+    await statistics.onClickTgBtn();
+    e.target.removeAttribute('disabled');
+    window.location.href = 'https://t.me/+-anOngXRXHw5Y2M6';
+  })
+
+  if (user && user?.name && user?.phone && user?.time) {
     const formData = new FormData();
+
     formData.append('Ismi', user?.name);
     formData.append('Telefon raqami', user?.phone);
     formData.append(`Ro'yxatdan o'tgan vaqti`, user?.time);
-    fetch('https://script.google.com/macros/s/AKfycbwk32Db9qSl3KFD7ZMtKzGKx1ldrX-O1_hxupJJZ0N5Z6gSJxQYVu2sDhfUE9g7rEm6/exec', {
-        method: 'POST',
-        body: formData
+    formData.append(`Foydalanuvchi ID`, statistics.userId);
+    formData.append(`Timestamp`, statistics.time?.toString());
+
+    let response = await fetch('https://script.google.com/macros/s/AKfycbwk32Db9qSl3KFD7ZMtKzGKx1ldrX-O1_hxupJJZ0N5Z6gSJxQYVu2sDhfUE9g7rEm6/exec', {
+      method: 'POST',
+      body: formData
     })
-        .then()
-        .then(() => {
-            localStorage.clear();
-        })
-} else {
-    localStorage.clear();
-}
+    await response.json();
+    await statistics.onRegister(user);
+    localStorage.removeItem('user');
+  } else {
+    localStorage.removeItem('user');
+  }
+})()
+
