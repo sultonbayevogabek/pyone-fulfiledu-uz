3572f7136356c75a7618d469e3ab90cdeca03f92
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-03-16 17:25:03 +0500

    commit

--- js/statistics.js
+++ js/statistics.js
@@ -1,7 +1,7 @@
 'use strict';
 
 class Statistics {
-  PROJECT_NAME = 'pyone-4-5-04-2025';
+  PROJECT_NAME = 'python-one-4-5-04-2025';
   DB_URL = 'https://webinar-statistics-default-rtdb.firebaseio.com/';
   ENTERED_DB = `${this.DB_URL}${this.PROJECT_NAME}-entered.json`;
   CLICKED_REG_BTN_DB = `${this.DB_URL}${this.PROJECT_NAME}-clicked-reg-btn.json`;
